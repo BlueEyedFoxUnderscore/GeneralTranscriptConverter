99eabfa839ba3bdccbbbc3a20b69c5b00295b943
Oxity <oxityy@proton.me>
2024-12-19 21:31:36 +0100

    Add first files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="generaltranscriptconverter",
+    version="0.1.0",
+    description="A python script that translates between RUMBLE VR notations.",
+    license="MIT",
+    author="Oxity",
+    author_email="oxityy@proton.me",
+    include_package_data=True,
+    package_data={
+        "gtc": ["notation_systems/*.json"],
+    },
+)
