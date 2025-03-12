f43d5b311de86d6fdc666532649df0ce3d0eced4
BlueEyedFoxUnderscore <pagzma09@gmail.com>
2025-03-12 11:55:36 -1000

    Commenting 5: The Commenter Strikes Back

--- gtc/gtc.py
+++ gtc/gtc.py
@@ -221,7 +221,6 @@ def flatten_system(system: dict[str, dict[str]|bool]) -> dict[str, str]:
 
 def yoink_bools(system:dict[str]) -> dict[str, bool]:
     # > Replaced system_bools with a dict for consistency's sake. Having a list of dicts is insane IMO.
-    # ? Just for my own curiosity, why was that? You could have made it a (str, bool) tuple.
     system_bools = {}
     # Sort for boolean values
     # > Replaced name of iterant "bools" with "key" as not all iterants were, in fact, bools - that's the point of this function
@@ -265,32 +264,43 @@ def translate(user_input:str, from_system, to_system):
     # If the key with identity "shiftstones/delimiter" is present in the string to translate, shiftstoning is true
     # > Flattened to an equivalent single identifier.
     shiftstoning = flattened_from_system["shiftstones/delimiter"] in processed_user_input
+    
     # Instansiate tokenized and detokenized lists
     input_translated_to_keys = []
     keys_translated_to_notation = []
+    
     # Instansiate exit signal boolean
     exit_signal = False
+
     # Instansiate previous number of instances variable
     previous_number_of_instances = 0
+    
     # While we haven't processed the whole string
     while start_index < len(processed_user_input) and end_index < len(processed_user_input) + 1:
         # Get current symbol
         symbol = processed_user_input[start_index:end_index]
+        
         # Filter the system for shiftstones
         filtered_from_system = filter_system_for_shiftstones(flattened_from_system)
+        
         # Iff the symbol is now the delimiter, we no longer need to filter.
         # > Flattened to equivalent logical statement.
         shiftstoning = (symbol == flattened_from_system["shiftstones/delimiter"])
+        
         # Begin matching symbol cases and define possible tokens.
         match len(symbol_instances := find_instances_in_system(filtered_from_system, symbol)):
+        
             # If there are no possible tokens
             case 0:
-                # If there are previous instances of this symbol
+        
+                # If there were previously more than one possible token
                 if previous_number_of_instances > 1:
                     # Move back one
                     end_index -= 1
-                    # Set symbol to smaller portion
+                    
+                    # Set symbol to token
                     symbol = processed_user_input[start_index:end_index]
+
                     # Handle doubled symbol
                     chosen_option = handle_doubled(symbol, [key for key, val in filtered_from_system.items() if val[:len(symbol)] == symbol])
                     match chosen_option:
@@ -298,13 +308,16 @@ def translate(user_input:str, from_system, to_system):
                         case "exit":
                             exit_signal = True
                             break
+
                         # If invalid, skip and move on
                         case "invalid":
                             continue
+
                         # If ignore, set previous_number_of_instances to 0 to continue.
                         case "ignore":
                             previous_number_of_instances = 0
                             pass
+                        
                         # Otherwise, add input (raw) to the list of tokens
                         case _:
                             input_translated_to_keys.append(chosen_option)
@@ -315,11 +328,12 @@ def translate(user_input:str, from_system, to_system):
                             previous_number_of_instances = 0
                             continue
                 
-                # Now, handle the invalid value
+                # Otherwise, handle the invalid value
                 match handle_invalid_value(symbol):
                     case "remove":
                         # Prompt for input
                         remove_all_instances_choice = input("All instances? [N/y] > ")
+
                         # > Strings are truthy, false only if completely empty. Shortened and clarified.
                         # If user says yes
                         if not remove_all_instances_choice or remove_all_instances_choice.lower()[0] != "y":
@@ -328,136 +342,258 @@ def translate(user_input:str, from_system, to_system):
                         else:
                             # Otherwise, remove all instances
                             processed_user_input = processed_user_input.replace(symbol, "")
+
+                        # Reset index to start
                         end_index = start_index + 1
                         continue
                     case "change":
+                        # Get symbol to replace with
                         replacement_symbol = input(f"Replace {symbol} with: ")
+                        
+                        # Ask if all instances should be replaced
                         change_all_instances_choice = input("All instances? [N/y] > ")
                         if len(change_all_instances_choice) == 0 or change_all_instances_choice.lower()[0] != "y":
-                            change_user_input = processed_user_input.replace(symbol, replacement_symbol, 1)
+                            # > Pretty sure you meant processed_user_input since change_user_input is never used, changed
+                            processed_user_input = processed_user_input.replace(symbol, replacement_symbol, 1)
                         else:
                             processed_user_input = processed_user_input.replace(symbol, replacement_symbol)
+                        # * Possible shorter version, less easy to understand
+                        # * processed_user_input= processed_user_input.replace(symbol, replacement_symbol, -1 if len(change_all_instances_choice) == 0 or change_all_instances_choice.lower()[0] != "y" else 1)
+
+                        # Reset to start index to reprocess
                         end_index = start_index + 1
                         continue
                     case "assign":
+                        # Get all parent keys (categories)
                         all_parent_keys = [key for key, value in from_system.items() if type(value) == dict]
+
+                        # Get assigned parent/child key from user input
                         assigned_parent_key = input(f"Insert the category of {symbol} ({", ".join(all_parent_keys)} or custom category): ")
                         assigned_child_key = input(f"Insert a key for {symbol}: ")
+
+                        # Generated full key
                         assigned_full_key = f"{assigned_parent_key}/{assigned_child_key}"
+
+                        # Add key
                         filtered_from_system.update({assigned_full_key: symbol})
+
+                        # Add to tokenized input
                         input_translated_to_keys.append(assigned_full_key)
                         reset_index_distance()
                         increment_indices()
                         continue
                     case "include":
                         try:
-                            characters_to_insert = int(input("Number of charaters to insert: "))
-                            if characters_to_insert > len(processed_user_input[end_index:]):
-                                characters_to_insert = len(processed_user_input[end_index:])
-                            if characters_to_insert <= -len(symbol):
-                                characters_to_insert = -len(symbol) + 1
+                            # Get number of insertion characters
+                            # > Added loop here so that typos can be corrected
+                            while(True):
+                                characters_to_insert = int(input("Number of charaters to insert: "))
+
+                                # Assert that insertion characters don't run past string
+                                if characters_to_insert > len(processed_user_input[end_index:]):
+                                    if(input(f"Warning, characters would override string, limited to less than {characters_to_insert} characters. Retry? [Y/n] ").lower() == "n" or "no"):
+                                        characters_to_insert = len(processed_user_input[end_index:])
+                                        break
+                                    else:
+                                        continue
+                                
+                                # Assert that number of characters is more than the length of the symbol
+                                if characters_to_insert <= -len(symbol):
+                                    if(input(f"Warning, characters less than current limit, limited to more than {characters_to_insert} characters. Retry? [Y/n] ").lower() == "n" or "no"):
+                                        characters_to_insert = -len(symbol) + 1
+                                        break
+                                    else:
+                                        continue
+                                if(input(f"Input number of {characters_to_insert} characters means that {symbol[start_index:end_index+characters_to_insert]} will be included. Are you sure? [Y/n]" == "n" or "no")):
+                                    continue
+                                else:
+                                    break
+
+                            # Move end_index by characters_to_insert
                             end_index += characters_to_insert
                             continue
                         except:
                             print(f"Invalid number.")
                             continue
                     case "ignore":
-                        randomized_key = "".join(random.sample("0123456789abcdefghijklmnopkrstuvwxyz", k=16))
+                        # Get a randomized key made of invisible variation selectors
+                        # > Changed this to Unicode variation selectors to avoid edge cases. Apologies to any computerized notations that use these.
+                        randomized_key = "".join(random.sample("\uFE00\uFE01\uFE02\uFE03\uFE04\uFE05\uFE06\uFE07\uFE08\uFE09\uFE0A\uFE0B\uFE0C\uFE0D\uFE0E\uFE0F", k=16))
+                        
+                        # Update with new randomized key
                         filtered_from_system.update({randomized_key: symbol})
+
+                        # Update with new randomized key
                         flattened_to_system.update({randomized_key: symbol})
                         continue
                     case "exit":
                         exit_signal = True
                         break
             case 1:
+                
+                # If symbol isn't in symbol_instances
                 if symbol != symbol_instances[0]:
+
+                    # If (we aren't at end) (increment end_index) else (reset previous_number_of_instances)
                     if end_index != len(input_notation):
                         end_index += 1
                         continue
                     else:
                         previous_number_of_instances = 0
                         continue
+                
+                # Append token to list of tokens
                 input_translated_to_keys.append(dict_find_key_by_value(filtered_from_system, symbol))
+
+                # Set previous number of instances to 1
                 previous_number_of_instances = 1
+
+                # Move on to next character
                 reset_index_distance()
                 increment_indices()
                 continue
             case _:
+                # If there is more than one possible token
                 if end_index == len(processed_user_input):
+
+                    # Prompt for user input
                     chosen_option = handle_doubled(symbol, [key for key, val in filtered_from_system.items() if val == symbol])
+                    
+                    # Match to the chosen option
                     match chosen_option:
                         case "exit":
                             exit_signal = True
                             break
+                        
+                        # Ignore symbol (don't translate)
                         case "ignore":
                             previous_number_of_instances = 0
                             continue
+
+                        # Else, set to chosen option
                         case _:
                             input_translated_to_keys.append(chosen_option)
                             reset_index_distance()
                             increment_indices()
                             continue
+                # Set previous_number_of_instances to (number of possible tokens)
                 previous_number_of_instances = len(symbol_instances)
+
+                # Increment indexes
                 end_index += 1
+
+                # Continue
                 continue
         if exit_signal:
+            # If exit was provided, break
             break
     if exit_signal:
-        return ""
+        # If exit was provided, stop function
+        # > Changed from return "" to return None because "" could be interpreted as a valid translated string
+        return None
+    # ============================== #
+    #                                #
+    #   BEGIN TRANSLATION TO SYSTEM  #
+    #                                #
+    # ============================== #
+    # Set translation_index to zero
     translation_index = 0
+
+    # Initialize previous_was_invalid
     previous_was_invalid = False
+
+    # While we haven't used all tokens
     while translation_index < len(input_translated_to_keys):
+
+        # If the previous input was valid
         if not previous_was_invalid:
-            key = input_translated_to_keys[translation_index]
+
+            # Token is current working key
+            # > Changed varible name from key to working_token to clarify usage
+            working_token = input_translated_to_keys[translation_index]
         try:
-            keys_translated_to_notation.append(flattened_to_system[key])
+            # Append the equivalent token to the notation string
+            keys_translated_to_notation.append(flattened_to_system[working_token])
             translation_index += 1
             previous_was_invalid = False
         except:
             print(f"Here is the currently translated portion of the notation:\n{"".join(keys_translated_to_notation)}")
-            match handle_invalid_key(key):
+
+            # Prompt for handling invalid key
+            match handle_invalid_key(working_token):
                 case "remove":
+                    # Ask if should remove all instances
                     change_all_instances_choice = input("All instances? [n/Y] > ")
-                    if len(change_all_instances_choice) == 0 or change_all_instances_choice.lower()[0] != "n":
+
+                    # If should remove all instances
+                    if not change_all_instances_choice or change_all_instances_choice.lower()[0] != "n":
+                        
+                        # Iterate over all tokens
                         for instance in input_translated_to_keys:
-                            if instance == key:
+
+                            # If (token is correct token for removal), remove token
+                            if instance == working_token:
                                 input_translated_to_keys.remove(instance)
                     else:
                         translation_index += 1
                 case "change":
+                    # Signal that previous token was invalid
                     previous_was_invalid = True
-                    changed_key = input(f"Change {key} to: ")
+
+                    # Prompt for new key
+                    changed_key = input(f"Change {working_token} to: ")
+
+                    # Ask if should replace all instances of this token
                     change_all_instances_choice = input("All instances? [n/Y] > ")
                     if len(change_all_instances_choice) == 0 or change_all_instances_choice.lower()[0] != "n":
-                        input_translated_to_keys = [changed_key if x == key else x for x in input_translated_to_keys]
+                        # Look for identical tokens and change
+                        input_translated_to_keys = [changed_key if x == working_token else x for x in input_translated_to_keys]
+
+                        #Signal resolved
                         previous_was_invalid = False
                         continue
                     else:
-                        key = changed_key
+                        working_token = changed_key
                 case "assign":
-                    flattened_to_system.update({key: f"{input(f"Assign a value to {key}: ")}"})
+                    # Assign to a new translated token
+                    flattened_to_system.update({working_token: f"{input(f"Assign a value to {working_token}: ")}"})
                 case "note":
+                    # Prompt user to ask if we should change all instances to notes
                     change_all_instances_choice = input("All instances? [n/Y] > ")
                     if len(change_all_instances_choice) == 0 or change_all_instances_choice.lower()[0] != "n":
-                        input_translated_to_keys = ["cadenceAndClarity/note" if x == key else x for x in input_translated_to_keys]
+                        # Look for identical tokens and change them
+                        input_translated_to_keys = ["cadenceAndClarity/note" if x == working_token else x for x in input_translated_to_keys]
                     else:
+                        # Set previous_was_invalid to True
                         previous_was_invalid = True
-                        key = "cadenceAndClarity/note"
+                        # Set the token to be a note
+                        working_token = "cadenceAndClarity/note"
                 case "exit":
+                    # If exit, then break out of loop
                     exit_signal = True
                     break
             continue
+        # If exit, then end
     if exit_signal:
-        return ""
+        # > Changed to return None because "" could be interpreted as a valid translation string
+        return None
+    
+    # Join all translated tokens
     translated_notation = "".join(keys_translated_to_notation)
+    
+    # Trim off whitespace
     if to_system["name"][0] == "text":
         if input_translated_to_keys[-1][:len("modifiers/")] != "modifiers/" and input_translated_to_keys[-1][:len("states/")] != "states/" and input_translated_to_keys[-1] != "cadenceAndClarity/note":
             translated_notation = translated_notation[:-2]
+    
+    # Add notes
     number_of_notes = 0
     for note_symbol in keys_translated_to_notation:
         if note_symbol == flattened_to_system["cadenceAndClarity/note"]:
             number_of_notes += 1
             translated_notation += f"\n{note_symbol}{input(f"Note {number_of_notes}: ")}"
+    
+    # Return fully translated notation
     return translated_notation
 
 if __name__ == "__main__":
