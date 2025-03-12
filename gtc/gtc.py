09506b527752d90ed0a88fe5df5752ff434261fe
BlueEyedFoxUnderscore <pagzma09@gmail.com>
2025-03-11 22:39:48 -1000

    Began tokenization process

--- gtc/gtc.py
+++ gtc/gtc.py
@@ -11,6 +11,7 @@
 # make a pull request or contact Oxity on Discord (.oxity).
 
 import os, json, random
+from typing import Optional
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 folder_path = os.path.join(script_dir, "notation_systems")
@@ -20,12 +21,12 @@ quit_words = ["exit", "close", "quit"]
 
 header = """
 ╔═══════════════════════════════════════════════════════╗
-║  ,.-</#@$>,      ,.-~=+>#$@>$#$(>*"`   ,..sDG#$       ║
-║ /|$      \%$     "'`    \#$#          /D<             ║
-║ |&%   ,.   '             |%|         <$?              ║
-║  \#$   \$.,              $#           CD\      ,$     ║
-║   `<&>,./.d             <>?             ec>#$#%/      ║
-║     `'*'`               `                             ║
+║  ,.-</#@$>,      ,.-~=+>#$@>$#$(>*"`   ,..<&%#$       ║
+║ /|$      \%$     "'`    \#$#          /%<             ║
+║ |&%   ,.   '             |%|         |$?              ║
+║  \#$   \$.,              $#           \$\      ,$     ║
+║   `<&>,./.d             <>?             <%>#$#%/      ║
+║     `'*'`               `                  ``         ║
 ║ G E N E R A L  T R A N S C R I P T  C O N V E R T E R ║
 ╚═════╤═══════════════════════════════════════════╤═════╝
       │  Made by 0xity (mostly) and BlueEyedFox_  │      
@@ -58,10 +59,11 @@ def load_charts():
     # Return the finalized list
     return json_list
 
-def dict_find_key_by_value(system: dict, value):
-    return system.keys()[system.values().index(value)]
+def dict_find_key_by_value(system: dict[str], value) -> str:
+    # > Oops! Forgot that dict_values don't have .index() function. Returned.
+    return system.keys()[list(system.values()).index(value)]
 
-def find_system_by_name(systems: dict[str], name):
+def find_system_by_name(systems: list[dict[str]], name: str) -> Optional[dict[str]]:
     try:
         for obj in systems:
             # For (Current system) in (Range of systems)
@@ -72,23 +74,31 @@ def find_system_by_name(systems: dict[str], name):
     except:
         return None
 
-def input_valid_system(systems, prompt):
+def input_valid_system(systems: dict, prompt: str) -> Optional[str]:
     user_input = input(prompt)
     # Quit if in quit words
-    # ? Oxity, is this needed? You can always quit a program with ctrl+C. - BlueEyedFox_
     if user_input in quit_words:
         return "exit"
-    # > Indexing an array is expensive, cut down on processor cycles here by condensing calls - BlueEyedFox_
+    # Find system
     system = find_system_by_name(systems, user_input)
+    # Iff system is None (not found), say nothing
     if system == None: print("Invalid system.")
-    return system if system != None else "invalid"
+    # > Changed to return None instead of "invalid" due to edge case of there being a notation system named "invalid"
+    # Return system
+    return system
 
-def find_instances_in_system(system, symbol):
-    all_values = []
+def find_instances_in_system(system: dict[str], symbol: str) -> list[str]:
+    # Initialize variables
+    # all_values = []
     all_instances = []
+    # For each value in values
+    # > Redid this so that it's shorter 
+    """
     for value in system.values():
-        all_values.append(value)
-    for instance in all_values:
+        # Append values
+        all_values.append(value)"""
+    c = [(type(x).__name__ == dict.__name__) for x in system.values()]
+    for instance in [*system.values()]:
         if instance[:len(symbol)] == symbol:
             all_instances.append(instance)
     return all_instances
@@ -118,6 +128,8 @@ def handle_invalid_value(symbol):
             return "ignore"
         case _:
             print("\nInvalid option.\n")
+
+
 
 def handle_doubled(symbol, keys):
     options = ""
@@ -165,245 +177,93 @@ def handle_invalid_key(key):
             print("\nInvalid option.\n")
             return "invalid"
 
-def process_system(system):
+def process_system(system: dict[str, dict[str]|bool]) -> dict[str, str]:
+    # Initiate variables
     processed_system = {}
+    # Get processed systems
     for obj in list(system.values()):
+        # Sort for dict objects
         if type(obj) == dict:
-            for child_key in list(obj.keys()):
+            # Get keys from dict, form compound key, and add compound key directly to processed_system
+            # > Replaced with list comprehension for more compact code
+            [processed_system.update({{f"{dict_find_key_by_value(system, obj)}/{child_key}": obj[child_key]}}) for child_key in list(obj.keys())]
+            """for child_key in list(obj.keys()):
+                # Get compound key from system
                 parent_key = dict_find_key_by_value(system, obj)
                 processed_key = f"{parent_key}/{child_key}"
-                processed_system.update({processed_key: obj[child_key]})
+                # Add these compound keys to the system
+                processed_system.update({processed_key: obj[child_key]})"""
+    # Return finalized system
     return processed_system
 
-def yoink_bools(system):
+def yoink_bools(system:dict[str]) -> dict[str, bool]:
     system_bools = []
     for bools in list(system.keys()):
         if type(system[bools]) == bool:
             system_bools.append({bools: system[bools]})
     return system_bools
 
-def translate(user_input, from_system, to_system):
-    start_index = 0
-    end_index = 1
-    def increment_indices():
-        nonlocal start_index, end_index
-        start_index += 1
-        end_index += 1
-    def reset_index_distance():
-        nonlocal start_index, end_index
-        start_index = end_index - 1
-    # define processed variable
-    processed_user_input = user_input
-    def filter_system_for_shiftstones(system):
-        filtered_system = system.copy()
-        for key in system.keys():
-            if shiftstoning:
-                if key[:len("shiftstones/")] != "shiftstones/":
-                    filtered_system.pop(key)
-            else:
-                if key[:len("shiftstones/")] == "shiftstones/":
-                    filtered_system.pop(key)
-        return filtered_system
-    processed_from_system = process_system(from_system)
-    processed_to_system = process_system(to_system)
-    if processed_from_system["shiftstones/delimiter"] in processed_user_input:
-        shiftstoning = True
-    else:
-        shiftstoning = False
-    input_translated_to_keys = []
-    keys_translated_to_notation = []
-    exit_signal = False
-    previous_number_of_instances = 0
-    while start_index < len(processed_user_input) and end_index < len(processed_user_input) + 1:
-        symbol = processed_user_input[start_index:end_index]
-        filtered_from_system = filter_system_for_shiftstones(processed_from_system)
-        if symbol == processed_from_system["shiftstones/delimiter"]:
-            shiftstoning = False
-        match len(symbol_instances := find_instances_in_system(filtered_from_system, symbol)):
-            case 0:
-                if previous_number_of_instances > 1:
-                    end_index -= 1
-                    symbol = processed_user_input[start_index:end_index]
-                    chosen_option = handle_doubled(symbol, [key for key, val in filtered_from_system.items() if val[:len(symbol)] == symbol])
-                    match chosen_option:
-                        case "exit":
-                            exit_signal = True
-                            break
-                        case "invalid":
-                            continue
-                        case "ignore":
-                            previous_number_of_instances = 0
-                            # So that the condition isn't met
-                            #   and it moves on to invalid symbol handling.
-                            pass
-                        case _:
-                            input_translated_to_keys.append(chosen_option)
-                            reset_index_distance()
-                            increment_indices()
-                            previous_number_of_instances = 0
-                            continue
-                match handle_invalid_value(symbol):
-                    case "remove":
-                        remove_all_instances_choice = input("All instances? [N/y] > ")
-                        if len(remove_all_instances_choice) == 0 or remove_all_instances_choice.lower()[0] != "y":
-                            processed_user_input = processed_user_input.replace(symbol, "", 1)
-                        else:
-                            processed_user_input = processed_user_input.replace(symbol, "")
-                        end_index = start_index + 1
-                        continue
-                    case "change":
-                        replacement_symbol = input(f"Replace {symbol} with: ")
-                        change_all_instances_choice = input("All instances? [N/y] > ")
-                        if len(change_all_instances_choice) == 0 or change_all_instances_choice.lower()[0] != "y":
-                            change_user_input = processed_user_input.replace(symbol, replacement_symbol, 1)
-                        else:
-                            processed_user_input = processed_user_input.replace(symbol, replacement_symbol)
-                        end_index = start_index + 1
-                        continue
-                    case "assign":
-                        all_parent_keys = [key for key, value in from_system.items() if type(value) == dict]
-                        assigned_parent_key = input(f"Insert the category of {symbol} ({", ".join(all_parent_keys)} or custom category): ")
-                        assigned_child_key = input(f"Insert a key for {symbol}: ")
-                        assigned_full_key = f"{assigned_parent_key}/{assigned_child_key}"
-                        filtered_from_system.update({assigned_full_key: symbol})
-                        input_translated_to_keys.append(assigned_full_key)
-                        reset_index_distance()
-                        increment_indices()
-                        continue
-                    case "include":
-                        try:
-                            characters_to_insert = int(input("Number of charaters to insert: "))
-                            if characters_to_insert > len(processed_user_input[end_index:]):
-                                characters_to_insert = len(processed_user_input[end_index:])
-                            if characters_to_insert <= -len(symbol):
-                                characters_to_insert = -len(symbol) + 1
-                            end_index += characters_to_insert
-                            continue
-                        except:
-                            print(f"Invalid number.")
-                            continue
-                    case "ignore":
-                        randomized_key = "".join(random.sample("0123456789abcdefghijklmnopkrstuvwxyz", k=16))
-                        filtered_from_system.update({randomized_key: symbol})
-                        processed_to_system.update({randomized_key: symbol})
-                        continue
-                    case "exit":
-                        exit_signal = True
-                        break
-            case 1:
-                if symbol != symbol_instances[0]:
-                    if end_index != len(input_notation):
-                        end_index += 1
-                        continue
-                    else:
-                        previous_number_of_instances = 0
-                        continue
-                input_translated_to_keys.append(dict_find_key_by_value(filtered_from_system, symbol))
-                previous_number_of_instances = 1
-                reset_index_distance()
-                increment_indices()
-                continue
-            case _:
-                if end_index == len(processed_user_input):
-                    chosen_option = handle_doubled(symbol, [key for key, val in filtered_from_system.items() if val == symbol])
-                    match chosen_option:
-                        case "exit":
-                            exit_signal = True
-                            break
-                        case "ignore":
-                            previous_number_of_instances = 0
-                            continue
-                        case _:
-                            input_translated_to_keys.append(chosen_option)
-                            reset_index_distance()
-                            increment_indices()
-                            continue
-                previous_number_of_instances = len(symbol_instances)
-                end_index += 1
-                continue
-        if exit_signal:
-            break
-    if exit_signal:
-        return ""
-    translation_index = 0
-    previous_was_invalid = False
-    while translation_index < len(input_translated_to_keys):
-        if not previous_was_invalid:
-            key = input_translated_to_keys[translation_index]
-        try:
-            keys_translated_to_notation.append(processed_to_system[key])
-            translation_index += 1
-            previous_was_invalid = False
-        except:
-            print(f"Here is the currently translated portion of the notation:\n{"".join(keys_translated_to_notation)}")
-            match handle_invalid_key(key):
-                case "remove":
-                    change_all_instances_choice = input("All instances? [n/Y] > ")
-                    if len(change_all_instances_choice) == 0 or change_all_instances_choice.lower()[0] != "n":
-                        for instance in input_translated_to_keys:
-                            if instance == key:
-                                input_translated_to_keys.remove(instance)
-                    else:
-                        translation_index += 1
-                case "change":
-                    previous_was_invalid = True
-                    changed_key = input(f"Change {key} to: ")
-                    change_all_instances_choice = input("All instances? [n/Y] > ")
-                    if len(change_all_instances_choice) == 0 or change_all_instances_choice.lower()[0] != "n":
-                        input_translated_to_keys = [changed_key if x == key else x for x in input_translated_to_keys]
-                        previous_was_invalid = False
-                        continue
-                    else:
-                        key = changed_key
-                case "assign":
-                    processed_to_system.update({key: f"{input(f"Assign a value to {key}: ")}"})
-                case "note":
-                    change_all_instances_choice = input("All instances? [n/Y] > ")
-                    if len(change_all_instances_choice) == 0 or change_all_instances_choice.lower()[0] != "n":
-                        input_translated_to_keys = ["cadenceAndClarity/note" if x == key else x for x in input_translated_to_keys]
-                    else:
-                        previous_was_invalid = True
-                        key = "cadenceAndClarity/note"
-                case "exit":
-                    exit_signal = True
-                    break
-            continue
-    if exit_signal:
-        return ""
-    translated_notation = "".join(keys_translated_to_notation)
-    if to_system["name"][0] == "text":
-        if input_translated_to_keys[-1][:len("modifiers/")] != "modifiers/" and input_translated_to_keys[-1][:len("states/")] != "states/" and input_translated_to_keys[-1] != "cadenceAndClarity/note":
-            translated_notation = translated_notation[:-2]
-    number_of_notes = 0
-    for note_symbol in keys_translated_to_notation:
-        if note_symbol == processed_to_system["cadenceAndClarity/note"]:
-            number_of_notes += 1
-            translated_notation += f"\n{note_symbol}{input(f"Note {number_of_notes}: ")}"
-    return translated_notation
 
-if __name__ == "__main__":
-    all_systems = load_charts()
-    while True:
-        try:
-            input_system = input_valid_system(all_systems, "From: ")
-            match input_system:
-                case "exit":
-                    break
-                case "invalid":
-                    continue
-
-            output_system = input_valid_system([system for system in all_systems if system != input_system], "To: ")
-            # Every system except input_system.
-            match output_system:
-                case "exit":
-                    break
-                case "invalid":
-                    continue
-
-            input_notation = input(f"From {input_system["name"][0]} to {output_system["name"][0]}: ")
-            if input_notation in quit_words:
+def tokenize(str: str, tokens: dict[str, list[str]]) -> list[str]:
+    preferred_tokens = {}
+    allowed_tokens = {}
+    token_lengths = []
+    # Populate preferred tokens list
+    [preferred_tokens.update({key: tokens.get(key)[1]}) for key in list(tokens.keys())]
+    # Populate allowed tokens list
+    [allowed_tokens.update({key: tokens.get(key)[-1]}) for key in list(tokens.keys())]
+    # Populate token lengths with both
+    [token_lengths.append(tokens.get(key)[1]) if key in token_lengths else token_lengths for key in list(tokens.keys())]
+    [token_lengths.append(tokens.get(key)[-1]) if key in token_lengths else token_lengths for key in list(tokens.keys())]
+    # We want to check for the longest tokens first, so we sort the list
+    token_lengths.sort()
+    # Unfortunately, sort is ascending in place, so we need to reverse as well
+    token_lengths = token_lengths[::-1]
+    index = 0
+    prev_indexes = []
+    prev_index_exclusions = {
+        0: [-1],
+        1: [-1]
+    }
+    possible_tokens = []
+    current_tokens = []
+    # Iterate over the whole list
+    while index < len(str):
+        possible_tokens = []
+        # Check each possible token length, starting with longer tokens
+        for length in token_lengths:
+            if length not in prev_index_exclusions.get(index) and str[index:index+length] in preferred_tokens:
+                # Add all possible tokens to list
+                [possible_tokens.append(preferred_tokens.get(x)) if preferred_tokens.get(x) else None for x in preferred_tokens.keys()]
+                # Update reference values
+                prev_indexes.append(i)
+                current_tokens.append(possible_tokens)
+                i += length
                 break
-            print(f"\n{translate(input_notation, input_system, output_system)}\n")
-        except Exception as e:
-            print(f"ERROR: {e}")
-            break
+        # If we weren't able to obtain a token
+        if(len(possible_tokens) == 0):
+            # Get previous index and remove it from list
+            prev_index = prev_indexes.pop()
+            # Get previous token length
+            prev_token_length = index - prev_index
+            # Iff (ID (index)) in (Exclusion list) doesn't exist
+            if (prev_index_exclusions.get(prev_index_exclusions[prev_index])) == None :
+                # Create it
+                prev_index_exclusions.update({prev_index: prev_token_length})
+            else:
+                # Update it
+                exclusions = prev_index_exclusions.get(prev_index_exclusions[prev_index])
+                exclusions.append(prev_token_length)
+                exclusions.sort()
+                exclusions = exclusions[::-1]
+            index = prev_indexes[-1]
+            current_tokens.pop()
+
+
+
+
+        
+
+def translate(user_input, from_system, to_system):
+    raise BaseException("Main function of this whole thing unimplemented as of late, check back in a few years and maybe it'll be done")
+    return None
